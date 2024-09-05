34f2c46e37481b6dbb269abe5e5a51f5e0a90f02
Rui Li <cucucucu4github@gmail.com>
2024-09-05 22:00:51 +0000

    Updated AccountDAO.java; Applied try-with-resources; Changed some method return type;

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -27,23 +27,21 @@ public class AccountDAO {
     public List<Account> getAllAccounts(){
 
         List<Account> accounts = new ArrayList<>();
+        String sql = "SELECT * FROM Account;";
 
-        try(Connection connection = ConnectionUtil.getConnection()){
+        try(Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);){
 
-            // prepare the query statement
-            String sql = "SELECT * FROM Account;";
-            PreparedStatement ps = connection.prepareStatement(sql);
-            
-            // execute the query, and then put all accounts in list
-            ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                int newId = rs.getInt("account_id");
-                String newUserName = rs.getString("username");
-                String newPassword = rs.getString("password");
-                Account newAccount = new Account(newId, newUserName, newPassword);
-                accounts.add(newAccount);
+            // execute the query in try-with resources, and then put all accounts in list
+            try(ResultSet rs = ps.executeQuery()) {
+                while(rs.next()){
+                    int newId = rs.getInt("account_id");
+                    String newUserName = rs.getString("username");
+                    String newPassword = rs.getString("password");
+                    Account newAccount = new Account(newId, newUserName, newPassword);
+                    accounts.add(newAccount);
+                }
             }
-
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
@@ -52,22 +50,23 @@ public class AccountDAO {
 
     // return a specific account by id
     public Account getAccountById(int id){
-        
-        try(Connection connection = ConnectionUtil.getConnection()){
 
-            // prepare the query statement
-            String sql = "SELECT * FROM Account WHERE account_id = ?;";
-            PreparedStatement ps = connection.prepareStatement(sql);
+        String sql = "SELECT * FROM Account WHERE account_id = ?;";
+        
+        try(Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);){
+
             ps.setInt(1, id);
             
             // execute the query, and then get the account
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()){
-                int newId = rs.getInt("account_id");
-                String newUserName = rs.getString("username");
-                String newPassword = rs.getString("password");
-                Account newAccount = new Account(newId, newUserName, newPassword);
-                return newAccount;
+            try(ResultSet rs = ps.executeQuery()){    
+                if (rs.next()){
+                    int newId = rs.getInt("account_id");
+                    String newUserName = rs.getString("username");
+                    String newPassword = rs.getString("password");
+                    Account newAccount = new Account(newId, newUserName, newPassword);
+                    return newAccount;
+                }
             }
         }catch(SQLException e){
             System.out.println(e.getMessage());
@@ -78,12 +77,12 @@ public class AccountDAO {
     // return a specific account by username
     // the reason I need this method is I do not know the id when user want to login/register
     public Account getAccountByUserName(String username){
-        
-        try(Connection connection = ConnectionUtil.getConnection()){
 
-            // prepare the query statement
-            String sql = "SELECT * FROM Account WHERE username = ?;";
-            PreparedStatement ps = connection.prepareStatement(sql);
+        String sql = "SELECT * FROM Account WHERE username = ?;";
+
+        try(Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);){
+
             ps.setString(1, username);
             
             // execute the query, and then get the account
@@ -105,23 +104,24 @@ public class AccountDAO {
     // return null if failure
     public Account insertAccount(String newUsername, String newPassword){
 
-        try(Connection connection = ConnectionUtil.getConnection()){
+        String sql = "INSERT INTO Account (username, password) VALUES (?, ?);";
 
-            // prepare the query statement
-            String sql = "INSERT INTO Account (username, password) VALUES (?, ?);";
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        try(Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
+            
             ps.setString(1, newUsername);
             ps.setString(2, newPassword);
             
             // execute the query, and then get the result;
             ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs.next()){
-                int generatedId = (int) rs.getLong(1);
-                Account newAccount = new Account(generatedId, newUsername, newPassword);
-                return newAccount;
+            try(ResultSet rs = ps.getGeneratedKeys()){
+                if (rs.next()){
+                    int generatedId = (int) rs.getLong(1);
+                    Account newAccount = new Account(generatedId, newUsername, newPassword);
+                    return newAccount;
+                }
             }
-
+    
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
@@ -129,69 +129,97 @@ public class AccountDAO {
         return null;
     }
 
-    // update an account password by id and password, return boolean.
-    public void updateAccountPasswordById(int id, String password){
-        try(Connection connection = ConnectionUtil.getConnection()){
+    // update an account password by id and password
+    // return boolean, true means update success, fail means update failure.
+    public boolean updateAccountPasswordById(int id, String password){
+        
+        String sql = "UPDATE Account SET password = ? WHERE account_id = ?;";
+
+        try(Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);){
 
             // prepare the query statement
-            String sql = "UPDATE Account SET password = ? WHERE account_id = ?;";
-            PreparedStatement ps = connection.prepareStatement(sql);
             ps.setString(1, password);
             ps.setInt(2, id);
             
             // execute the query, and then get the result;
-            ps.executeUpdate();
+            // the rows updated should only be 1, since all id are unique
+            int updatedRowsNum = ps.executeUpdate();
+            if(updatedRowsNum == 1) return true;
+
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
+
+        return false;
     }
 
-    // update an account password by username and password, return boolean.
-    public void updateAccountPasswordByUsername(String username, String password){
-        try(Connection connection = ConnectionUtil.getConnection()){
+    // update an account password by username and password.
+    // return boolean, true means update success, fail means update failure.
+    public boolean updateAccountPasswordByUsername(String username, String password){
+        
+        String sql = "UPDATE Account SET password = ? WHERE username = ?;";
 
-            // prepare the query statement
-            String sql = "UPDATE Account SET password = ? WHERE username = ?;";
-            PreparedStatement ps = connection.prepareStatement(sql);
+        try(Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);){
+
             ps.setString(1, password);
             ps.setString(2, username);
             
             // execute the query, and then get the result;
-            ps.executeUpdate();
+            // the rows updated should only be 1, since all username are unique
+            int updatedRowsNum = ps.executeUpdate();
+            if(updatedRowsNum == 1) return true;
+
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
+        return false;
     }
 
     // delete account by username
-    public void deleteAccountByUsername(String username){
-        try(Connection connection = ConnectionUtil.getConnection()){
+    // return boolean, true means delted one rows; false means failure;
+    public boolean deleteAccountByUsername(String username){
 
-            // prepare the query statement
-            String sql = "DELETE FROM Account WHERE username = String";
-            PreparedStatement ps = connection.prepareStatement(sql);
+        String sql = "DELETE FROM Account WHERE username = String";
+
+        try(Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);){
+
             ps.setString(1, username);
             
             // execute the query, and then get the result;
-            ps.executeUpdate();
+            // the rows updated should only be 1, since all username are unique
+            int rowsDeletedNum = ps.executeUpdate();
+            if(rowsDeletedNum == 1) return true;
+
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
+
+        return false;
     }
 
     // delete account by id
-    public void deleteAccountById(int id){
-        try(Connection connection = ConnectionUtil.getConnection()){
+    // return boolean, true means delted one rows; false means failure;
+    public boolean deleteAccountById(int id){
 
-            // prepare the query statement
-            String sql = "DELETE FROM Account WHERE account_id = ?";
-            PreparedStatement ps = connection.prepareStatement(sql);
+        String sql = "DELETE FROM Account WHERE account_id = ?";
+
+        try(Connection connection = ConnectionUtil.getConnection();
+            PreparedStatement ps = connection.prepareStatement(sql);){
+
             ps.setInt(1, id);
             
             // execute the query, and then get the result;
-            ps.executeUpdate();
+            // the rows updated should only be 1, since all username are unique
+            int rowsDeletedNum = ps.executeUpdate();
+            if(rowsDeletedNum == 1) return true;
+
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
+
+        return false;
     }
 }
