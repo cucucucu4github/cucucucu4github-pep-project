d3494dfd0cfa0e78d9a53177ff9a7dc5ec49e580
Rui Li <cucucucu4github@gmail.com>
2024-09-05 07:04:18 +0000

    updated AccountDAO, changed some design due to the README requirements.

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -29,9 +29,12 @@ public class AccountDAO {
         List<Account> accounts = new ArrayList<>();
 
         try(Connection connection = ConnectionUtil.getConnection()){
+
+            // prepare the query statement
             String sql = "SELECT * FROM Account;";
             PreparedStatement ps = connection.prepareStatement(sql);
             
+            // execute the query, and then put all accounts in list
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
                 int newId = rs.getInt("account_id");
@@ -40,6 +43,7 @@ public class AccountDAO {
                 Account newAccount = new Account(newId, newUserName, newPassword);
                 accounts.add(newAccount);
             }
+
         }catch(SQLException e){
             System.out.println(e.getMessage());
         }
@@ -48,14 +52,81 @@ public class AccountDAO {
 
     // return a specific account by id
     public Account getAccountById(int id){
+        
+        try(Connection connection = ConnectionUtil.getConnection()){
+
+            // prepare the query statement
+            String sql = "SELECT * FROM Account WHERE account_id = ?;";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            
+            // execute the query, and then get the account
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                int newId = rs.getInt("account_id");
+                String newUserName = rs.getString("username");
+                String newPassword = rs.getString("password");
+                Account newAccount = new Account(newId, newUserName, newPassword);
+                return newAccount;
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
-    // insert a new account, return boolean
-    // true: update successful
-    // false: update failure
-    public Boolean insertAccount(Account account){
-        return true;
+    // return a specific account by username
+    // the reason I need this method is I do not know the id when user want to login/register
+    public Account getAccountByUserName(String username){
+        
+        try(Connection connection = ConnectionUtil.getConnection()){
+
+            // prepare the query statement
+            String sql = "SELECT * FROM Account WHERE username = ?;";
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, username);
+            
+            // execute the query, and then get the account
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                int newId = rs.getInt("account_id");
+                String newUserName = rs.getString("username");
+                String newPassword = rs.getString("password");
+                Account newAccount = new Account(newId, newUserName, newPassword);
+                return newAccount;
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    // insert a new account by username and password, return updated Account
+    // return null if failure
+    public Account insertAccount(String newUsername, String newPassword){
+
+        try(Connection connection = ConnectionUtil.getConnection()){
+
+            // prepare the query statement
+            String sql = "INSERT INTO Account (username, password) VALUES (?, ?);";
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, newUsername);
+            ps.setString(2, newPassword);
+            
+            // execute the query, and then get the result;
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()){
+                int generatedId = (int) rs.getLong(1);
+                Account newAccount = new Account(generatedId, newUsername, newPassword);
+                return newAccount;
+            }
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        // if we didn't return eariler when we hit the re.next(), then we update failure.
+        return null;
     }
 
     // update an account, return boolean.
